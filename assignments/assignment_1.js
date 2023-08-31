25ad0276195566e8326f434286da824b42045f69
Dilay Canıtez <dilay.canitez@one37id.com>
2023-08-31 21:01:32 +0300

    updates to functions

--- assignments/assignment_1.js
+++ assignments/assignment_1.js
@@ -2,8 +2,17 @@ const args = process.argv.slice(2);
 
 const calculateCircleArea = (radius) => {
   if (radius) {
-    console.log(`Radius: ${radius * 1}`);
-    console.log(`Circle Area: ${radius * 1 * 2 * Math.PI}`);
+    console.log(
+      `${isNaN(radius) ? "Radius should be a number" : `Radius: ${radius * 1}`}`
+    );
+
+    console.log(
+      `${
+        isNaN(radius)
+          ? "Circle area could not be calculated."
+          : `Circle Area: ${Math.pow(parseInt(radius), 2) * Math.PI}`
+      }`
+    );
   } else {
     console.error(
       "Error 🚩: Please specify a radius to calculate the circle area near the folder name e.g. node assignment_1.js 5."
